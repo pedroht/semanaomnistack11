be1c3a55de0110707eb66c01b468fb2fece55a94
Pedro Henrique <pedro.henrique.torres@hotmail.com>
2020-03-29 15:21:51 -0300

    Configurando testes

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,8 +3,6 @@ const cors = require("cors");
 const { errors } = require("celebrate");
 const routes = require("./routes");
 
-const port = 3333;
-
 const app = express();
 
 app.use(cors());
@@ -12,6 +10,4 @@ app.use(express.json());
 app.use(routes);
 app.use(errors());
 
-app.listen(port, () => {
-  console.log(`Server is running at port ${port}`);
-});
+module.exports = app;
